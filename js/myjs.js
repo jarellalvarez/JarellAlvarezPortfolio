ccf4a7fd3defea37b06406505259c02c76b2bb7c
Jarell Alvarez <A00930217@ad.bcit.ca>
2017-01-27 11:22:46 -0800

    added a template for jquery

---
+++ js/myjs.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+// ===== Sticky Header ===== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#header').fadeIn(400);    // Fade in the arrow
+    } else {
+        $('#header').fadeOut(400);   // Else fade out the arrow
+    }
+});
+
+
+
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+
+
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+    }); 
