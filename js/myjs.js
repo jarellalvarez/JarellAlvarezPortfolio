40d26629d2fe0a1640942414a4f4812ff5afc3b8
hipsterbrew <jarell.alvarez@hotmail.com>
2020-03-14 17:51:14 -0700

    website redesign

--- js/myjs.js
+++ js/myjs.js
@@ -1,81 +1,11 @@
-$(document).ready(function(){
-  $("#webDLink").click(function() {
-    if ($(".designHov").css("display") == "block"){
-$(".designHov").css("display", "none");
-    }
-    else
-    {
-      $(".designHov").css("display", "block");
-        $(".storyHov").css("display", "none");
-            $(".devHov").css("display", "none");
-    }
-  });
-
-
-  $("#SLink").click(function() {
-    if ($(".storyHov").css("display") == "block"){
-$(".storyHov").css("display", "none");
-    }
-    else
-    {
-      $(".storyHov").css("display", "block");
-      $(".designHov").css("display", "none");
-          $(".devHov").css("display", "none");
-    }
-  });
-
-
-
-$("#webDevLink").click(function() {
-  if ($(".devHov").css("display") == "block"){
-$(".devHov").css("display", "none");
-  }
-  else
-  {
-    $(".devHov").css("display", "block");
-    $(".storyHov").css("display", "none");
-        $(".designHov").css("display", "none");
-  }
-});
-
-});
-
-
-
-// ===== Sticky Header =====
-/*$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#header').fadeIn(400);    // Fade in the arrow
-    } else {
-        $('#header').fadeOut(400);   // Else fade out the arrow
-    }
-});
-
-
-
-
-// ===== Scroll to Top ====
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
-});
-
-
-$('#return-to-top').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-});
-    });
-
-
-  function myFunction() {
-    var x = document.getElementById("linksinMenu");
-    if (x.className === "linksinMenu") {
-        x.className += " responsive";
-    } else {
-        x.className = "linksinMenu";
-    }*/
+//$(document).ready(function(){
+//  $("#myworkbutton").mouseenter(function(){
+//      $('#menu').toggleClass('hidden');
+//  });
+//  $("#myworkbutton").mouseleave(function(){
+//      $('#menu').toggleClass('hidden');
+//  });
+//   $("#menu").mouseleave(function(){
+//       $('#menu').toggleClass('hidden');
+//   });
+//});
